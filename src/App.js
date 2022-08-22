a8e37bee3f86aab8a36e258c6122d69ce636f95f
Gingaling <nycgingsf@hotmail.com>
2022-08-22 04:06:26 -0700

    Committing files; rebuilding repository

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import { Routes, Route, Navigate } from 'react-router-dom';
+import data from './data.json';
+import artObjects from './artObjects.json';
+import Container from 'react-bootstrap/Container';
+import CarouselContainer from './CarouselContainer';
+import Navigation from './Navigation';
+import About from './About';
+import Gallery from './Gallery';
+import Search from './Search';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [searchOptions, setSearchOptions] = useState({
+		key: process.env.REACT_APP_RIJKS_KEY,
+		url: 'https://www.rijksmuseum.nl/api/en/',
+		numberOfResults: 50
+	});
+	const [galleryImages, setGalleryImages] = useState(artObjects);
+	return (
+		<Container>
+			<Navigation />
+			<main>
+				<Routes>
+					<Route path="/home" element={<CarouselContainer data={data} />} />
+					<Route path="/about" element={<About />} />
+					<Route
+						path="/gallery"
+						element={
+							<Gallery images={galleryImages} searchOptions={searchOptions} />
+						}
+					/>
+					<Route path="*" element={<Navigate to="/home" />} />
+					<Route
+						path="/search"
+						element={<Search searchOptions={searchOptions} />}
+					/>;
+				</Routes>
+			</main>
+		</Container>
+	);
 }
 
 export default App;
